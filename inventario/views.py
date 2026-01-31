fc8cba272f77eb8e540bb9e103ff6893ff04a06a
Martin <mrtncsto@gmail.com>
2026-01-31 13:53:27 -0400

    clase 6: cruds

--- inventario/views.py
+++ inventario/views.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from inventario.models import Producto, Categoria
@@ -8,9 +8,19 @@ from inventario.serializers import ProductoSerializer, CategoriaSerializer, Prod
 # Create your views here.
 
 
+
 class InventarioListView(APIView):
     def get(self, request):
+        nombre = self.request.query_params.get("nombre")
+        categoria = self.request.query_params.get("categoria")
         data = Producto.objects.all()
+
+        if nombre:
+            data = data.filter(nombre__icontains=nombre)
+
+        if categoria:
+            data = data.filter(categoria__nombre__icontains=categoria)
+
         serializer = ProductoSerializer(data, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     def post(self, request):
@@ -29,16 +39,51 @@ class InventarioDetailView(APIView):
         except Producto.DoesNotExist:
             return Response(data={"message": "el producto no existe"},
                      status=status.HTTP_404_NOT_FOUND)
-    def put(self, request):
-        pass
-    def patch(self, request):
-        pass
-    def delete(self, request):
-        pass
+    def put(self, request, pk=None):
+        #Primero es consultar si el registro que se busca existe
+        #Si no existe devuelvo un error
+        #Serializar lo que viene en la peticion request.data
+        #Editan dicho registro
+        #Devuelven una respuesta si se edito con exito
+        #Si algo sale mal se devuelve un error
+        try:
+            data = Producto.objects.get(pk=pk)
+            serializer = ProductoPostSerializer(data, data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response({"message":"producto editado"}, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Producto.DoesNotExist:
+            return Response({"message":"No se encontro el producto"}, status=status.HTTP_404_NOT_FOUND)
+        
+    def patch(self, request, pk=None):
+        try:
+            data = Producto.objects.get(pk=pk)
+            serializer = ProductoPostSerializer(data, data=request.data, partial=True)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response({"message":"producto editado"}, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Producto.DoesNotExist:
+            return Response({"message":"No se encontro el producto"}, status=status.HTTP_404_NOT_FOUND)
+        
+    def delete(self, request, pk=None):
+        #Primero es consultar si el registro que se busca existe
+        #Si no existe devuelvo un error
+        #Si existe se borra el producto
+        try:
+            data = Producto.objects.get(pk=pk)
+            data.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Producto.DoesNotExist:
+            return Response({"message":"No se encontro el producto"}, status=status.HTTP_404_NOT_FOUND)
 
 class CategoriaListView(APIView):
     def get(self, request):
         data = Categoria.objects.all()
+        nombre = self.request.query_params.get("nombre")
+        if nombre:
+            data = data.filter(nombre__icontains=nombre)
         serializer = CategoriaSerializer(data, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     def post(self, request):
@@ -57,9 +102,32 @@ class CategoriaDetailView(APIView):
         except Categoria.DoesNotExist:
             return Response(data={"message": "la categoria no existe"},
                      status=status.HTTP_404_NOT_FOUND)
-    def put(self, request):
-        pass
-    def patch(self, request):
-        pass
-    def delete(self, request):
-        pass
+    def put(self, request, pk=None):
+        try:
+            data = Categoria.objects.get(pk=pk)
+            serializer = CategoriaSerializer(data, data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response({"message":"categoria editada"}, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Categoria.DoesNotExist:
+            return Response({"message":"No se encontro la categoria"}, status=status.HTTP_404_NOT_FOUND)
+        
+    def patch(self, request, pk=None):
+        try:
+            data = Categoria.objects.get(pk=pk)
+            serializer = CategoriaSerializer(data, data=request.data, partial=True)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response({"message":"categoria editada"}, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Categoria.DoesNotExist:
+            return Response({"message":"No se encontro la categoria"}, status=status.HTTP_404_NOT_FOUND)
+        
+    def delete(self, request, pk=None):
+        try:
+            data = Categoria.objects.get(pk=pk)
+            data.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Categoria.DoesNotExist:
+            return Response({"message":"No se encontro la categoria"}, status=status.HTTP_404_NOT_FOUND)
